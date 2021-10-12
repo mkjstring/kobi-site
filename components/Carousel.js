94e1ecd1237ca54fbc5bfd52abd21f6ba6f0c4a0
mjstring <mk.string@pm.me>
2021-10-12 17:42:52 -0500

    tested css changes and tests for framer animate

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { UncontrolledCarousel, Row, Col } from "reactstrap";
+import { motion } from "framer-motion";
 
 const items = [
   {
-    src: ("/img/4-59.jpg"),
+    src: "/img/4-59.jpg",
     altText: "Slide 1",
     caption: "",
     header: "",
     key: "1",
   },
   {
-    src: ("/img/4-60.jpg"),
+    src: "/img/4-60.jpg",
     altText: "Slide 2",
     caption: "",
     header: "",
     key: "2",
   },
   {
-    src: ("/img/4-61.jpg"),
+    src: "/img/4-61.jpg",
     altText: "Slide 3",
     caption: "",
     header: "",
@@ -26,11 +27,16 @@ const items = [
 ];
 
 const Carousel = () => (
-  <Row>
-    <Col md="8" className="mx-auto">
-      <UncontrolledCarousel items={items} />
-    </Col>
-  </Row>
+  <motion.div className='carousel'
+  animate={{z:-100}}
+  transition={{ ease: "easeOut", duration: 2 }}
+  >
+    <Row>
+      <Col md="8" className="mx-auto">
+        <UncontrolledCarousel className="protect-image" items={items} />
+      </Col>
+    </Row>
+  </motion.div>
 );
 
 export default Carousel;
