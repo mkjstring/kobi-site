5adfd3dbb6fd3497b86b06801bf2fa14b4e6949a
mjstring <mk.string@pm.me>
2021-10-13 14:32:52 -0500

    removed bootstrap from all but navbar

--- components/Footer.js
+++ components/Footer.js
@@ -1,73 +1,89 @@
-import React from 'react';
-import { Container, Row, Col } from "reactstrap";
+import React from "react";
+
 const Footer = () => {
   const links = [
-    { id : 1, title : "Feature",
-      child : [
-          { title : "LOREM IPSUM", link : "/" },
-          { title : "LOREM IPSUM", link : "/" },
-          { title : "LOREM IPSUM", link : "/" },
-          { title : "LOREM IPSUM", link : "/" }
-      ]
+    {
+      id: 1,
+      title: "Feature",
+      child: [
+        { title: "LOREM IPSUM", link: "/" },
+        { title: "LOREM IPSUM", link: "/" },
+        { title: "LOREM IPSUM", link: "/" },
+        { title: "LOREM IPSUM", link: "/" },
+      ],
     },
-    { id : 2, title : "About Us",
-      child : [
-          { title : "Contact Us", link : "/" },
-          { title : "FAQs", link : "/" },
-          { title : "Privacy Policy", link : "/" },
-      ]
+    {
+      id: 2,
+      title: "About Us",
+      child: [
+        { title: "Contact Us", link: "/" },
+        { title: "FAQs", link: "/" },
+        { title: "Privacy Policy", link: "/" },
+      ],
     },
   ];
-  
+
   return (
     <section className="footer section">
-      <Container>
-        <Row>
-          <Col lg={4}>
-              <div className="mb-4">
-                <p className="text-muted mt-4 mb-2">email@email.com</p>
-                <h6 className="text-muted font-weight-normal">+99 1234-5678-9</h6>
-              </div>
-          </Col>
-          <Col lg={8}>
-            <Row>
-              {
-                links.map((link, key) =>
-                  <Col key={key} md={4}>
-                    <h6 className="text-dark mb-3">{link.title}</h6>
-                    <ul className="list-unstyled company-sub-menu">
-                      {
-                        link.child.map((fLink, key) =>
-                          <li key={key}><a href={fLink.link}>{fLink.title}</a></li>
-                        )
-                      }
-                    </ul>
-                  </Col>
-                )
-              }
-              
-              <Col md={4}>
+      <div className="container">
+        <div className="row">
+          <div className="column">
+            <div>
+              <p>email@email.com</p>
+              <h6>+99 1234-5678-9</h6>
+            </div>
+          </div>
+          <div className="column" lg={8}>
+            <div className="row">
+              {links.map((link, key) => (
+                <div className="column" key={key} md={4}>
+                  <h6 className="text-dark mb-3">{link.title}</h6>
+                  <ul className="list-unstyled company-sub-menu">
+                    {link.child.map((fLink, key) => (
+                      <li key={key}>
+                        <a href={fLink.link}>{fLink.title}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+
+              <div className="column">
                 <h6 className="text-dark mb-3">Our Address</h6>
-                <p className="text-muted f-14">1234 Lorem Ipsum dummy text, 12345</p>
+                <p className="text-muted f-14">
+                  1234 Lorem Ipsum dummy text, 12345
+                </p>
                 <h6 className="text-muted pb-2">Email: email@email.com</h6>
                 <ul className="list-unstyled footer-social-list mt-4">
-                  <li className="list-inline-item"><a href="#"><i className="mdi mdi-facebook"></i></a></li>
-                  <li className="list-inline-item"><a href="#"><i className="mdi mdi-instagram"></i></a></li>
-                  <li className="list-inline-item"><a href="#"><i className="mdi mdi-linkedin"></i></a></li>
+                  <li className="list-inline-item">
+                    <a href="#">
+                      <i className="mdi mdi-facebook"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#">
+                      <i className="mdi mdi-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#">
+                      <i className="mdi mdi-linkedin"></i>
+                    </a>
+                  </li>
                 </ul>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-<Row className="mt-5">
-          <Col md={12}>
-            <div className="text-center text-muted">
-              <p className="mb-0 f-15">2020 © Name. Design by Name</p>
+              </div>
             </div>
-          </Col>
-        </Row>
-      </Container>
+          </div>
+        </div>
+        <div className="row">
+          <div className='column'>
+            <div>
+              <p>2020 © Name. Design by Name</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
-}
+};
 export default Footer;
