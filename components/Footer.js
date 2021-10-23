f05f85e4b6a1ac77c77bc79faa5b8d2af3597557
mjstring <mk.string@pm.me>
2021-10-23 16:54:59 -0500

    added gallery compoenent

--- components/Footer.js
+++ components/Footer.js
@@ -2,76 +2,19 @@ import React from "react";
 import SocialFollow from "./social-media";
 
 const Footer = () => {
-  const links = [
-    {
-      id: 1,
-      title: "Feature",
-      child: [
-        { title: "LOREM IPSUM", link: "/" },
-        { title: "LOREM IPSUM", link: "/" },
-        { title: "LOREM IPSUM", link: "/" },
-        { title: "LOREM IPSUM", link: "/" },
-      ],
-    },
-    {
-      id: 2,
-      title: "About Us",
-      child: [
-        { title: "Contact Us", link: "/" },
-        { title: "FAQs", link: "/" },
-        { title: "Privacy Policy", link: "/" },
-      ],
-    },
-  ];
-
   return (
     <section className="footer section">
       <div className="container">
         <div className="row">
-          <div className="column">
-            <div>
-              <p>email@email.com</p>
-              <h6>+99 1234-5678-9</h6>
-            </div>
-          </div>
           <div className="column" lg={8}>
             <div className="row">
-              {links.map((link, key) => (
-                <div className="column" key={key} md={4}>
-                  <h6 className="text-dark mb-3">{link.title}</h6>
-                  <ul className="list-unstyled company-sub-menu">
-                    {link.child.map((fLink, key) => (
-                      <li key={key}>
-                        <a href={fLink.link}>{fLink.title}</a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-
               <div className="column">
-                <h6 className="text-dark mb-3">Our Address</h6>
-                <p className="text-muted f-14">
+                <h6 className="mb-3">Contact</h6>
+                <h6>+99 1234-5678-9</h6>
+                <p className="f-14">
                   1234 Lorem Ipsum dummy text, 12345
                 </p>
-                <h6 className="text-muted pb-2">Email: email@email.com</h6>
-                <ul className="list-unstyled footer-social-list mt-4">
-                  <li className="list-inline-item">
-                    <a href="#">
-                      <i className="mdi mdi-facebook"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="#">
-                      <i className="mdi mdi-instagram"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="#">
-                      <i className="mdi mdi-linkedin"></i>
-                    </a>
-                  </li>
-                </ul>
+                <h6 className="pb-2">Email: email@email.com</h6>
               </div>
             </div>
           </div>
@@ -79,7 +22,6 @@ const Footer = () => {
         <div className="row">
           <div className='column'>
             <div>
-              <p>2020 © Name. Design by Name</p>
             </div>
           </div>
         </div>
--- components/Gallery.js
+++ components/Gallery.js
@@ -1,70 +1,12 @@
-import React from 'react';
-const FeatureBox = (props) => {
+import React from "react";
+import { render } from "react-dom";
+import Gallery from "react-photo-gallery";
+import { photos } from "./photos";
+
+/* popout the browser and maximize to see more columns! -> */
+const NewGallery = () => {
   return (
-    <>
-    {
-      props.features.map((feature, key) =>
-      (feature.id % 2 !== 0) ?
-        <div className='container'>
-          <div className='column'>
-            <div>
-              <img src={feature.img} alt="" className="img-fluid d-block mx-auto"/>
-            </div>
-          </div>
-            <div className='column' md={{size:6, offset:1}}>
-              <div className="mt-5 mt-sm-0 mb-4">
-                <div className="my-4">
-                  <i className={feature.icon}></i>
-                </div>
-                <h5 className="text-dark font-weight-normal mb-3 pt-3">{feature.title}</h5>
-                <p className="text-muted mb-3 f-15">{feature.desc}</p>
-                <a href={feature.link} className="f-16 text-warning">Read More <span className="right-icon ml-2">&#8594;</span></a>
-              </div>
-            </div>
-        </div>
-      :
-      <div className="row">
-        <div className='column'>
-          <div>
-            <div>
-              <i></i>
-            </div>
-            <h5>{feature.title}</h5>
-            <p >{feature.desc}</p>
-            <a href={feature.link} className="f-16 text-warning">Read More <span className="right-icon ml-2">&#8594;</span></a>
-          </div>
-        </div>
-        <div className="column">
-          <div>
-            <img src={feature.img} alt="" className="img-fluid d-block mx-auto"/>
-          </div>
-        </div>
-      </div>
-      )
-    }
-    </>
-  );
+    <Gallery className="gallery" photos={photos} direction={"column"} />
+  )
 }
-const Gallery = () => {
-const features = [
-    {id : 1, img : "./images/45.png", title : "LOREM IPSUM", desc : "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", link : "/"},
-    {id : 2, img : "./images/Group Members.png", title : "LOREM IPSUM", desc : "Sed perspiciatis unde omnis natus error voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo excepturi sint occaecati cupiditate architecto.", link : "/"},
-    {id : 3, img : "./images/45.png", title : "LOREM IPSUM", desc : "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", link : "/"},
-  ];
-return (
-    <section className="section" id="feature">
-      <div className='container'>
-        <div className='row' >
-          <div className='column'>
-            <div>
-              <h3><span className="text-warning">GALLERY</span></h3>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
-            </div>
-          </div>
-        </div>
-        <FeatureBox features={features} />
-      </div>
-    </section>
-  );
-}
-export default Gallery;
+export default NewGallery;
