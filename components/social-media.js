a95168c9e2ef3857731167178cf6b43a9892be5d
mjstring <mk.string@pm.me>
2021-10-26 00:24:45 -0500

    fixed css issue with social media add

--- components/social-media.js
+++ components/social-media.js
@@ -9,29 +9,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function SocialFollow() {
   return (
-    <div className="social-container">
-      <ul>
-        <li className="facebook">
-          <a href="#">
-            <i className="fa fa-facebook fa-2x" aria-hidden="true"></i>
-          </a>
-        </li>
-        <li className="twitter">
-          <a href="#">
-            <i className="fa fa-twitter fa-2x" aria-hidden="true"></i>
-          </a>
-        </li>
-        <li className="instagram">
-          <a href="https://www.instagram.com/kobimathis/">
-            <i className="fa fa-instagram fa-2x" aria-hidden="true"></i>
-          </a>
-        </li>
-        <li className="whatsapp">
-          <a href="#">
-            <i className="fa fa-whatsapp fa-2x" aria-hidden="true"></i>
-          </a>
-        </li>
-      </ul>
-    </div>
+    <section className="social-container">
+      <div>
+        <ul>
+          <li className="facebook">
+            <a href="#">
+              <i className="fa fa-facebook fa-2x" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li className="twitter">
+            <a href="#">
+              <i className="fa fa-twitter fa-2x" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li className="instagram">
+            <a href="https://www.instagram.com/kobimathis/">
+              <i className="fa fa-instagram fa-2x" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li className="whatsapp">
+            <a href="#">
+              <i className="fa fa-whatsapp fa-2x" aria-hidden="true"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
